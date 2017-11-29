9d808fe4c99b05178e780fd8d9c913919cc5df0a
Jeff Niu <jeffniu22@gmail.com>
2017-11-29 01:27:38 -0800

    Fix missing import in cmd

--- cmd/trackergraph/main.go
+++ cmd/trackergraph/main.go
@@ -2,16 +2,8 @@ package main
 
 import (
 	"jdash/app"
-	"jdash/trumptracker"
-	"time"
-	"jdash/config"
-	"fmt"
 )
 
 func main() {
 	app.Init()
-	lookbehindTime := time.Now().Unix() - int64(app.Config.Number[config.FIRESTORE_TRUMP_LOOKBACK] * config.SEC_IN_HRS)
-	it := trumptracker.GetDataIteratorSince(lookbehindTime)
-	resultMap := trumptracker.ParseDataIterator(it)
-	fmt.Println(resultMap)
 }
